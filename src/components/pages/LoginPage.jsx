aa5ec35e1b602abf9261dbc60a6e5f75cd2f5a6d
Kimberley Polypheme <kim.polypheme@gmail.com>
2024-01-01 18:15:59 +0100

    ✨ F01: LOGIN > Ajout titre et formulaire

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,6 +1,32 @@
+import { useState } from "react"
 
 export default function LoginPage() {
+  //state
+  const [inputValue, setInputValue] = useState("")
+
+  //comportements
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    alert(`Bonjour ${inputValue}`)
+    setInputValue("")
+  }
+  const handleChange = (event) => {
+    setInputValue(event.target.value)
+   }
+
+  //affichage
   return (
-    <div>LoginPage</div>
+    <div>
+      <h1>Bienvenue chez nous </h1>
+      <h2>Connectez-vous !</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input value={inputValue}
+               onChange={handleChange}
+               type="text"
+               placeholder="Entrez votre prénom"
+               required/>
+        <button>Mon espace</button>
+      </form>
+    </div>
   )
 }
