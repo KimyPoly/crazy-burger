c9c1631613fb60dc86568ad762cb04732e03fa57
Kimberley Polypheme <kim.polypheme@gmail.com>
2024-01-11 20:24:18 +0100

    💄 F05 : ORDER > Item > ajout style card

--- src/components/pages/order/Item.jsx
+++ src/components/pages/order/Item.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { theme } from "../../../theme"
 
 export default function Item({ id, imageSource, title, price, quantity, isAvailable, isAdvertised }) {
   return (
@@ -11,16 +12,18 @@ export default function Item({ id, imageSource, title, price, quantity, isAvaila
           <p>{price}</p>
           <button>Ajouter</button>
         </div>
-        
+
       </div>
     </ItemStyled>
   )
 }
  const ItemStyled = styled.div`
-  border: 1px solid black;
+  border: 1px solid ${theme.colors.background_white};
   height: 330px;
   width: 240px;
   color: black;
+  border-radius: ${theme.borderRadius.extraRound};
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2);
     img {
       height: 145px;
       width: 200px;
