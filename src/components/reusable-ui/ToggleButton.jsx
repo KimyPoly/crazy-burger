f80378ed6507db852fa9cb6ad79859a1ffaa98bb
Kimberley Polypheme <kim.polypheme@gmail.com>
2024-01-13 19:00:06 +0100

    ⚰️ F06: Suppression code inutile

--- src/components/reusable-ui/ToggleButton.jsx
+++ src/components/reusable-ui/ToggleButton.jsx
@@ -65,7 +65,6 @@ const ToggleButtonStyled = styled.div`
 
     // text inside the switch button (for checked and unchecked)
     &.toggle + label:after {
-      /* border: 1px solid blue; */
       width: 150px;
       text-align: center;
       z-index: 2;
@@ -89,7 +88,6 @@ const ToggleButtonStyled = styled.div`
 
     &.toggle:not(:checked) + label {
       background-color: ${theme.colors.background_dark};
-      /* text-align: right; */
     }
 
     // text label when not checked
